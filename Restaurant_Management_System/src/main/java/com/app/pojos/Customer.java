3e32aa0671762ca98ed60900e9af694a9f7ac1e6
Aniket Gawande <pgdac2022@gmail.com>
2023-02-21 16:55:29 +0530

    package structure uploaded

---
+++ Restaurant_Management_System/src/main/java/com/app/pojos/Customer.java
@@ -0,0 +1,18 @@
+package com.app.pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer {
+	private String email;
+	private String password;
+
+}
