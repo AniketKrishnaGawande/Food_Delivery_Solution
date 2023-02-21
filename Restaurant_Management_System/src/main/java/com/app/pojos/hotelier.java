e89c2089f55458125e2916d31c606dfcf11c11f9
SwanandKale <kaleswanand280101@gmail.com>
2023-02-21 17:25:11 +0530

    Hotelier and Menus

--- Restaurant_Management_System/src/main/java/com/app/pojos/hotelier.java
+++ Restaurant_Management_System/src/main/java/com/app/pojos/hotelier.java
@@ -1,38 +1,36 @@
 package com.app.pojos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-
-@Table(name="User_tbl")
+@Entity
+@Table(name="hotels")
 @NoArgsConstructor 
 @AllArgsConstructor 
 @Getter 
 @Setter
-@ToString(exclude = "password")
-public class User extends BaseEntity {
+public class hotelier extends BaseEntity{
 	@Column(length = 20)
-	private String firstName;
-	
-	@Column(length = 20)
-	private String lastName;
+	private String hName;
 	
 	@Column(length = 20)
-	private String password;
+	private String hAddress;
 	
-	@Column(length = 20)
-	private String email;
+	private int rating;
 	
+	@OneToMany()
+	private List<hotel_menu> menus=new ArrayList<>();
 	
-	private Role role;
-	
-	
+	// menu relation pending
 
 }
